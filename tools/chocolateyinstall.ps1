126599315ced3f32b16948da31093784c7b8a547
Tim Dunnington <accounts@timshouse.com>
2021-04-09 07:02:34 -0500

    Updated packages to 2.5.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $packageArgs = @{
 	packageName   = 'ivy'
 	unzipLocation = $toolsDir
-	url           = 'http://mirror.cc.columbia.edu/pub/software/apache//ant/ivy/2.4.0/apache-ivy-2.4.0-bin.zip'
+	url           = 'https://mirror.olnevhost.net/pub/apache//ant/ivy/2.5.0/apache-ivy-2.5.0-bin.zip'
 	softwareName  = 'ivy*'
-	checksum      = '86b581c7be6358e7e98ec76968d9aac8'
-	checksumType  = 'md5'
+	checksum      = '8a0bffab2c9225c2e761adf36e4f17eea2cd1876b5a6295d929569943bbcc47519a005c78328584852528d8ac71f1b47f7098490307e5d487b3676aad6d4eacd'
+	checksumType  = 'sha512'
 }
 
 if(!(Test-Path $env:ANT_HOME)) {
@@ -17,4 +17,4 @@ if(!(Test-Path $env:ANT_HOME)) {
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Copy-Item ($toolsDir + "\apache-ivy-2.4.0\ivy-2.4.0.jar") ($env:ANT_HOME + "\lib\ivy-2.4.0.jar")
+Copy-Item ($toolsDir + "\apache-ivy-2.5.0\ivy-2.5.0.jar") ($env:ANT_HOME + "\lib\ivy-2.5.0.jar")
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -2,5 +2,5 @@
 
 $packageName = 'ivy'
 
-Uninstall-ChocolateyZipPackage $packageName apache-ivy-2.4.0-bin.zip
-Remove-Item ($env:ANT_HOME + "\lib\ivy-2.4.0.jar")
+Uninstall-ChocolateyZipPackage $packageName apache-ivy-2.5.0-bin.zip
+Remove-Item ($env:ANT_HOME + "\lib\ivy-2.5.0.jar")
